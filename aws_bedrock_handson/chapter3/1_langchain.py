d7a29c7d5b0b5fe6378c955acde40067385de304
yuichkun <yuichkun@gmail.com>
2024-07-05 03:08:47 +0900

    chore: Add langchain and streamlit dependencies

---
+++ aws_bedrock_handson/chapter3/1_langchain.py
@@ -0,0 +1,18 @@
+from langchain_aws import ChatBedrock
+from langchain_core.messages import HumanMessage, SystemMessage
+
+chat = ChatBedrock(
+    model_id="anthropic.claude-3-5-sonnet-20240620-v1:0",
+    model_kwargs={
+        "max_tokens": 1000,
+    },
+)
+
+messages = [
+    SystemMessage(content="Act as an alien"),
+    HumanMessage(content="What is human?"),
+]
+
+response = chat.invoke(messages)
+
+print(response.content)
